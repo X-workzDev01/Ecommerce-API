16dddbb07992bbbd833f4cc9db1a4ce46f1ef29b
Netzwerk <devnetzwerk@gmail.com>
2024-06-04 12:27:52 +0530

    4 June, 24

--- ecommerce/src/main/java/com/netzwerk/ecommerce/serv/UserServ.java
+++ ecommerce/src/main/java/com/netzwerk/ecommerce/serv/UserServ.java
@@ -1,17 +1,23 @@
 package com.netzwerk.ecommerce.serv;
 
 import com.netzwerk.ecommerce.dto.UserModel;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UserServ {
 
     private static List<UserModel> users = new ArrayList<>();
 
+    private final PasswordEncoder passwordEncoder;
+
     public void register(UserModel user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         users.add(user);
     }
 
