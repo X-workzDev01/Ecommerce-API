dd58b176064d5aa19a66ed962bc21c00307cc714
Netzwerk <devnetzwerk@gmail.com>
2024-06-07 10:39:20 +0530

    7 June, 24

--- ecommerce/src/main/java/com/netzwerk/ecommerce/controller/UserController.java
+++ ecommerce/src/main/java/com/netzwerk/ecommerce/controller/UserController.java
@@ -1,36 +1,36 @@
-//package com.netzwerk.ecommerce.controller;
-//
-//import com.netzwerk.ecommerce.dto.UserModel;
-//import com.netzwerk.ecommerce.serv.UserServ;
-//import lombok.Data;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//@Controller
-//@RequiredArgsConstructor
-//@Data
-//public class UserController {
-//
-//    private final UserServ service;
-//
-//    @GetMapping("/login")
-//    public String getLoginPage(){
-//        return "login_page";
-//    }
-//
-//    @GetMapping("/registration")
-//    public String getRegistrationPage(Model model){
-//        model.addAttribute("user",new UserModel());
-//        return "registration_page";
-//    }
-//
-//    @PostMapping("/registration")
-//    public String registerUser(@ModelAttribute UserModel user){
-//        service.register(user);
-//        return "redirect:/login_page?success";
-//    }
-//}
+package com.netzwerk.ecommerce.controller;
+
+import com.netzwerk.ecommerce.dto.UserModel;
+import com.netzwerk.ecommerce.serv.UserServ;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Data
+public class UserController {
+
+    private final UserServ service;
+
+    @GetMapping("/login")
+    public String getLoginPage(){
+        return "login_page";
+    }
+
+    @GetMapping("/registration")
+    public String getRegistrationPage(Model model){
+        model.addAttribute("user",new UserModel());
+        return "registration_page";
+    }
+
+    @PostMapping("/registration")
+    public String registerUser(@ModelAttribute UserModel user){
+        service.register(user);
+        return "redirect:/login_page?success";
+    }
+}
--- ecommerce/src/main/java/com/netzwerk/ecommerce/serv/UserServ.java
+++ ecommerce/src/main/java/com/netzwerk/ecommerce/serv/UserServ.java
@@ -1,30 +1,30 @@
-//package com.netzwerk.ecommerce.serv;
-//
-//import com.netzwerk.ecommerce.dto.UserModel;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@Service
-//@Data
-//public class UserServ {
-//
-//    private static List<UserModel> users = new ArrayList<>();
-//
-//    private final PasswordEncoder passwordEncoder;
-//
-//    public void register(UserModel user){
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        users.add(user);
-//    }
-//
-//    public UserModel findByLogin (String login){
-//        return users.stream().filter(user -> user.getUsername().equals(login))
-//                .findFirst()
-//                .orElse(null);
-//    }
-//}
+package com.netzwerk.ecommerce.serv;
+
+import com.netzwerk.ecommerce.dto.UserModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Data
+public class UserServ {
+
+    private static List<UserModel> users = new ArrayList<>();
+
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(UserModel user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        users.add(user);
+    }
+
+    public UserModel findByLogin (String login){
+        return users.stream().filter(user -> user.getUsername().equals(login))
+                .findFirst()
+                .orElse(null);
+    }
+}
