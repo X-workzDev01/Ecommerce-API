161471116596106bf4ce38fa247a4a7a6cf32e74
Netzwerk <devnetzwerk@gmail.com>
2024-06-11 16:20:55 +0530

    11 June, 24

--- ecommerce/src/main/java/com/netzwerk/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/netzwerk/ecommerce/EcommerceApplication.java
@@ -1,18 +1,40 @@
 package com.netzwerk.ecommerce;
 
 import com.netzwerk.ecommerce.controller.EcommController;
+import com.squareup.okhttp.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.io.IOException;
 
 @SpringBootApplication
 
 public class EcommerceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		System.out.println("starting app");
-		SpringApplication.run(EcommerceApplication.class, args);
+	//	SpringApplication.run(EcommerceApplication.class, args);
+
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("text/xml ");
+
+		RequestBody body = RequestBody.create(mediaType, "Request XML");
+
+		Request request = new Request.Builder()
+
+				.url("http://localhost:9000/").post(body)
+
+				.addHeader("content-type", "text/xml")
+
+				.addHeader("cache-control", "no-cache")
+
+				.build();
+
+		Response response = client.newCall(request).execute();
+		System.out.println(response);
 		System.out.println("Running app");
 	}
 
